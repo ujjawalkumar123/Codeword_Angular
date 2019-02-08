1e1b8b7f014b5aa83b7da0ca347d0908d11880af
ujjwalkumar1212 <31749848+ujjwalkumar1212@users.noreply.github.com>
2019-02-07 21:47:06 -0600

    Removed unwanted codes

--- views/src/app/add-user/add-user.component.ts
+++ views/src/app/add-user/add-user.component.ts
@@ -16,6 +16,7 @@ export class AddUserComponent implements OnInit {
   errFlag = false;
   adduser = '';
   isFileUploaded = false;
+  // isDuplicate ;
   codeWordCount = 0;
   constructor(public dialog: MatDialog, public dialogRef: MatDialogRef<AddUserComponent>,
     @Inject(MAT_DIALOG_DATA) public data,
@@ -31,7 +32,6 @@ export class AddUserComponent implements OnInit {
     let files = event.target.files[0];
     let data = new FormData();
     data.append('file', files)
-
     this.codeWordSetService
       .previewFiles(data)
       .subscribe((res: any) => {
@@ -39,10 +39,20 @@ export class AddUserComponent implements OnInit {
         this.isFileUploaded = true;
         this.codeWordCount = res.count;
         this.tcodeWordSetData = res.data;
+        // this.isDuplicate = function(res: any){
+        //       for(var i=0; i<res.count-1; i++){
+        //         for(var j=i; j<res.count-1; j++){
+        //           if(res.data[i] == res.data[j+1]){
+        //             return true
+        //           }
+                
+        //         }
+        //       }
+        // }
+
       },
       err => {
         console.log(err)
-        // alert("Invalid credentials");
       })
   }
 
@@ -69,20 +79,15 @@ export class AddUserComponent implements OnInit {
             },
             err => {
               console.log(err)
-              // alert("Invalid credentials");
             })
         },
         err => {
           console.log(err)
-          // alert("Invalid credentials");
         })
-      // this.router.navigate(['/user'])
-
     } else {
       this.errFlag = true;
       data.reset
     }
-
   }
 
   rowClicked(row: any): void {
@@ -90,24 +95,15 @@ export class AddUserComponent implements OnInit {
     this.dialogRef.close()
   }
 
-  // onNoClick(): void {
-  //   this.dialogRef.close()}
-
   openRulesDialog(): void {
     const dialogRef = this.dialog.open(HintDialog, {
       width: '350px',
-      // data: {name: this.name, animal: this.animal}
     });
-
     // dialogRef.afterClosed().subscribe(result => {
     //   console.log('The dialog was closed');
       // this.animal = result});
-    ;
-  }
-
+    }
 }
-
-
 
 @Component({
   selector: 'snack-bar-component-add-user',
@@ -125,11 +121,7 @@ export class AddUserSnackBarComponent { }
   templateUrl: 'hint-dialog.html',
 })
 export class HintDialog {
-
-
   // constructor(
   //   public dialogRef: MatDialogRef<HintDialog>,
   //   ) {}
-   
- 
   }
---
+++ views/src/app/services/dashboard.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class CodewordsetService {
+
+
+    constructor(private httpclient: HttpClient) { }
+
+    CreateCourse() {}
+
+    handleFileUpload() {}
+
+    getStartDate() {}
+
+    loadCourseModel() {}
+
+    fetchCourseList() {}
+
+    getCourseName() {}
+
+    deleteCourseKey() {}
+
+    
+
+
+}
